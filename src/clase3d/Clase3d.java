b15a2eb6da364b045232850176ecba6ed5fd20d3
fmora122 <pfmoras@gmail.com>
2023-06-07 20:05:38 -0600

    first draft

---
+++ src/clase3d/Clase3d.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase3d;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fmora
+ */
+public class Clase3d {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String myNum;
+        int multi;
+        multi = 0;
+        //myNum = JOptionPane.showInputDialog("Esbriba un Numero\n");
+        //multi = Integer.parseInt(myNum);
+        for (int counter = 20; counter >= 20 && counter <= 30; counter++) {
+            System.out.println(counter+" al cuadrado es "+(counter*counter));
+        }
+
+    }
+    
+}
